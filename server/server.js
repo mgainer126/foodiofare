b93844f505e5a511fb69b174eae13a7728b5ef8c
Matt Gainer <matt.gainer126@hotmail.com>
2021-08-15 18:06:34 -0400

    basic server structure set up

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const cors = require("cors");
+const vendorLocations = require("./routes/vendorLocations");
+
+app.use(cors());
+
+app.use(express.json());
+
+//This below makes the pages in the public folder accessabile
+//by the browser. In this example there is a HTML document
+//that when you put localhost:8080 the html website serves up
+app.use(express.static("public"));
+
+app.use((req, _res, next) => {
+  console.log("Request:");
+  console.log("- Path:", req.path);
+  console.log("- Time:", new Date());
+  next();
+});
+
+app.use("/", vendorLocations);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
