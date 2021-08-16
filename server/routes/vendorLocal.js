677c6133f15c13d90a024807d6912d1b528a6775
Matt Gainer <matt.gainer126@hotmail.com>
2021-08-15 23:02:15 -0400

    added app express and appp use json

--- server/routes/vendorLocal.js
+++ server/routes/vendorLocal.js
@@ -4,6 +4,17 @@ const router = express.Router();
 // const videos = require("../data/videos.json");
 const fs = require("fs");
 const path = require("path");
+const app = express();
 const knex = require("knex")(require("../knexfile").development);
+app.use(express.json());
+
+app.get("/knex/users/", (req, res) => {
+  knex
+    .select("*")
+    .from("users")
+    .then((users) => {
+      res.status(200).json(users);
+    });
+});
 
 module.exports = router;
