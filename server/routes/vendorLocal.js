1159cec6d07640ed837c3826af049f4ed03aea54
Matt Gainer <matt.gainer126@hotmail.com>
2021-11-17 12:52:05 -0500

    removed some of not needed files on server side

--- server/routes/vendorLocal.js
+++ server/routes/vendorLocal.js
@@ -1,13 +1,8 @@
 const express = require("express");
-// const { userInfo } = require("os");
 const router = express.Router();
-// const knex = require("knex")(require("../knexfile").development);
-// const VendorController = require("../controller/createVendor");
 const database = require("../database");
-// const VendorService = require("../service/vendor");
 const app = express();
 app.use(express.json());
-// const Vendor = require("../models/vendors");
 
 router.get("/vendor", async (req, res) => {
   const results = await database.promise().query("SELECT * FROM vendorsinfo");
@@ -71,25 +66,5 @@ router.put("/:id", (req, res) => {
     }
   }
 });
-
-// router.put("/:id", (req, res) => {
-//   console.log("5");
-//   knex
-//     .select()
-//     .from("foodiofare1.vendorsinfo")
-//     .where({ vendorid: req.params.id })
-//     .update({
-//       addnum: req.body.addnum,
-//       streetname: req.body.streetname,
-//       streettype: req.body.streettype,
-//       city: req.body.city,
-//       province: req.body.province,
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       res.status(200).json(data);
-//     })
-//     .catch((err) => res.send("error getting vendor data"));
-// });
 
 module.exports = router;
